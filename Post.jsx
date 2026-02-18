f10ff3384f38494177adb223744cf0f54a34ff15
Dhayanand R <dhayanand0402@gmail.com>
2026-02-18 10:23:34 +0530

    Program 10

---
+++ Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptatibus eaque sed tempora, sequi eos magnam fugit vero quibusdam inventore perferendis consequatur obcaecati suscipit numquam, odit consectetur architecto ad voluptatem dolore. Perspiciatis atque magnam quo nesciunt totam, accusantium odit vero.</p>
+    </div>
+  )
+}
+
+export default Post
